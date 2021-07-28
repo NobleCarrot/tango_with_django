672931131d9b7f462bc73e4de7780f7f295f715a
carrot <huyw0721@outlook>
2021-07-28 19:38:58 +0100

    chapter 3, chapter 4 finished, because I restart, so this two step is fast

---
+++ rango/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from rango import views
+from django.urls import path
+
+app_name = 'rango'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about')
+]
